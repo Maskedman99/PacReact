60bb02f6c8a22dc2cd7d8d306dd8261e98de0186
Rohit <rohitmpaul@gmail.com>
2020-11-13 21:06:19 +0530

    add instruction to move pacman

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,6 +12,9 @@ const Menu = () => {
           </b>
           to resume the game.
         </div>
+        <div>
+          Use <b>W</b>, <b>A</b>, <b>S</b>, <b>D</b> or the arrow keys to control the pacman.
+        </div>
       </div>
     </div>
   );
